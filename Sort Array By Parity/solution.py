a30272323579ce1eb0415fd085703af9e2981855
Bryan Rojas <bryroj14@gmail.com>
2019-01-24 11:00:03 -0800

    905. Sort Array By Parity Completed

---
+++ Sort Array By Parity/solution.py
@@ -0,0 +1,21 @@
+def sortArrayByParity(A):
+    start, end = 0, len(A) - 1
+
+    while start < end:
+        m, n = A[start], A[end]
+
+        if m % 2 == 1 and n % 2 == 0:
+            A[start], A[end] = n, m
+        elif m % 2 == 1:
+            end -= 1
+        elif n % 2 == 0:
+            start += 1
+        else:
+            start += 1
+            end -= 1
+
+    return A
+
+A = [3,1,2,4]
+
+print(sortArrayByParity(A))
